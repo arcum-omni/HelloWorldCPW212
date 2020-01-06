11d48b5905976e0410947ccc68960a44a9836215
arcum-omni <hut8dev@gmail.com>
2020-01-06 13:50:55 -0800

    Added user greeting

--- HelloWorldCPW212/Program.cs
+++ HelloWorldCPW212/Program.cs
@@ -8,9 +8,13 @@ namespace HelloWorldCPW212
 {
     class Program
     {
+        /// <summary>
+        /// Comment
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Welcome!");
         }
     }
 }
